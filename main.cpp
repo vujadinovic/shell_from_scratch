afa6258fb442af287e79ff735b9554be660c1219
Stefan Vujadinovic <59982660+vujadinovic@users.noreply.github.com>
2025-11-08 16:49:47 +0100

    Implemented echo and changed function checking

--- main.cpp
+++ main.cpp
@@ -2,9 +2,26 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+
+void exit_shell(const vector<string>& args) {
+  if(args.size() == 1)
+    exit(0);
+  else if(stoi(args[0]) == 0 || stoi(args[0]) == 1) 
+    exit(stoi(args[0]));
+  else 
+    cout << "Invalid argument." << endl;
+}
+
+void echo(const vector<string>& args) {
+  for(string arg : args){
+    cout << arg << " ";
+  }
+  cout << endl;
+}
 
 
 int main() {
@@ -12,27 +29,37 @@ int main() {
   cout << unitbuf;
   cerr << unitbuf;
 
+  unordered_map<string, void(*)(const vector<string>&)> BUILTINS = {
+    {"exit", exit_shell},
+    {"echo", echo}
+  };
+
   while(true) {
     cout << "$ ";
+
+    // GET COMMAND INPUT
     string commandInput;
     getline(cin, commandInput);
     istringstream commandStream(commandInput);
+
+    // PARSE COMMAND
     string command;
     commandStream >> command;
+
+    // PARSE ARGUMENTS
     vector<string> args;
     while(commandStream) {
       string argument;
       commandStream >> argument;
       args.push_back(argument); 
     }
-    if(command == "exit") {
-      if(stoi(args[0]) == 0 || stoi(args[0]) == 1) 
-        exit(stoi(args[0]));
-      else 
-        cout << "Invalid argument." << endl;
-    }
-    else {
+
+    // CHECK FUNCTION
+    if(BUILTINS.find(command) == BUILTINS.end()) 
       cout << command << ": not found" << endl;
+        
+    else {
+      BUILTINS[command](args);
     }
     
   }
