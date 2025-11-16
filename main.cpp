61e40e4b957594b5364c259ca711eb1207c0564d
Stefan Vujadinovic <59982660+vujadinovic@users.noreply.github.com>
2025-11-16 12:39:27 +0100

    Finished path support for type function

--- main.cpp
+++ main.cpp
@@ -5,11 +5,7 @@
 #include <unordered_map>
 #include <filesystem>
 
-#ifdef _WIN32
-    const char PATH_SEPARATOR = ';';
-#else
-    const char PATH_SEPARATOR = ':';
-#endif
+
 
 
 namespace fs = std::filesystem;
@@ -42,6 +38,35 @@ void echo(const vector<string>& args) {
   cout << endl;
 }
 
+vector<string> splitPath(string pathString) {
+  #ifdef _WIN32
+    const char PATH_SEPARATOR = ';';
+  #else
+    const char PATH_SEPARATOR = ':';
+  #endif
+
+    vector<string> directories;
+    size_t start = 0;
+    size_t position = 0;
+    while((position = pathString.find(PATH_SEPARATOR, start)) != std::string::npos) {
+      directories.push_back(pathString.substr(start, position - start));
+      start = position + 1;
+    }
+    directories.push_back(pathString.substr(start));
+    return directories;
+}
+
+bool isExecutable(fs::path& pathPath) {
+  #ifdef _WIN32
+    return fs::exists(pathPath) && (pathPath.extension() == ".exe" || 
+                              pathPath.extension() == ".bat" || 
+                              pathPath.extension() == ".cmd");
+  #else
+    return fs::exists(pathPath) && fs::is_regular_file(pathPath) && 
+                                ((fs::status(pathPath).permissions() & fs::perms::owner_exec) != fs::perms::none);
+  #endif
+}
+
 void type(const vector<string>& args) {
   if (args.empty()) {
         cout << "type: missing argument\n";
@@ -50,7 +75,7 @@ void type(const vector<string>& args) {
   const string command = args[0];
 
   if (BUILTINS.find(command) != BUILTINS.end()) {
-        cout << command << " is a shell builtin\n";
+        cout << command << " is a shell builtin" << endl;
         return;
   }
   else {
@@ -59,9 +84,32 @@ void type(const vector<string>& args) {
       cout << command << ": not found" << endl;  
       return;
     }
+
     string pathString(path);
-    // TO DO
-  }
+    vector<string> directories = splitPath(pathString);
+
+    for(const string& directory : directories) {
+      fs::path fullPath = fs::path(directory) / command;
+      #ifdef __WIN32
+      vector<string> extensions = {".exe", ".bat", ".cmd"};
+        for (string extension : extensions) {
+            fs::path fullExtension = fullPath;
+            fullExtension += extension;
+            if (fs::exists(fullExtension)) {
+                cout << command << " is " << fullExtension.string() << endl;
+                return;
+            }
+        }
+      #else
+      if (isExecutable(fullPath)) {
+            cout << command << " is " << fullPath.string() << endl;
+            return;
+      }
+      #endif
+    }
+  
+
+}
 
 
   cout << command << ": not found" << endl;  
